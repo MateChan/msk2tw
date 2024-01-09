a4035251e9d94643df886f8b3ae31b54c4b216c7
cffnpwr <cffnpwr@gmail.com>
2024-01-09 08:57:53 +0000

    画像をアップロードする機能を追加

--- index.ts
+++ index.ts
@@ -1,59 +1,80 @@
-import { Hono } from 'https://deno.land/x/hono@v3.4.1/mod.ts'
-import { load } from 'https://deno.land/std@0.198.0/dotenv/mod.ts'
-import { Payload } from './types.ts'
-import { tweet } from './twitter.ts'
+import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
+import { load } from "https://deno.land/std@0.198.0/dotenv/mod.ts";
+import { Payload } from "./types.ts";
+import { tweet, uploadMediaFromURL } from "./twitter.ts";
 
 const timelog = (text: string, content?: string) => {
-    console.log(`${new Date().toLocaleString('ja-JP', { timeZone: 'JST' })
-        }: ${text}\n${content?.replace(/^/gm, '> ') ?? ''}`)
-}
+  console.log(
+    `${new Date().toLocaleString("ja-JP", { timeZone: "JST" })}: ${text}\n${
+      content?.replace(/^/gm, "> ") ?? ""
+    }`,
+  );
+};
 
-const env = await load()
+const env = await load();
 const [secret, authToken, ct0] = [
-    'MISSKEY_WEBHOOK_SECRET',
-    'TWITTER_AUTH_TOKEN',
-    'TWITTER_CT0'
-].map(key => Deno.env.get(key) ?? env[key] ?? '')
+  "MISSKEY_WEBHOOK_SECRET",
+  "TWITTER_AUTH_TOKEN",
+  "TWITTER_CT0",
+].map((key) => Deno.env.get(key) ?? env[key] ?? "");
 
-const app = new Hono()
+const app = new Hono();
 
-app.post('/', async c => {
-    const requestSecret: string = c.req.header('X-Misskey-Hook-Secret') ?? ''
-    if (requestSecret !== secret) {
-        const msg = 'wrong secret'
-        timelog(msg)
-        return c.text(msg, 403)
-    }
-    const payload: Payload = await c.req.json()
-    const { text, cw, visibility, localOnly, files } = payload.body.note
-    if (localOnly) {
-        const msg = `post visibility: "local only"`
-        timelog(msg)
-        return c.text(msg, 403)
-    }
-    if (visibility !== 'public' && visibility !== 'home') {
-        const msg = `post visibility: "${visibility}"`
-        timelog(msg)
-        return c.text(msg, 403)
-    }
-    const filesStr: string = (files ?? []).reduce((p: string, c) => {
-        return `${p}\n${c.type.includes('image') ? '🖼' : '📄'} ${c.url}`
-    }, '')
-    const tweetContent = (cw ? `${cw}...\n\n` : '') + (text ?? '') + filesStr
-    if (!tweetContent) {
-        const msg = 'empty content'
-        timelog(msg)
-        return c.text(msg, 403)
-    }
-    const res = await tweet(tweetContent, authToken, ct0)
-    if (res.status === 200) {
-        const msg = 'successfully tweeted'
-        timelog(msg, tweetContent)
-        return c.text(msg, 200)
-    }
-    const msg = 'error while tweeting'
-    timelog(msg, tweetContent)
-    return c.text(msg, 403)
-})
+app.post("/", async (c) => {
+  const requestSecret: string = c.req.header("X-Misskey-Hook-Secret") ?? "";
+  if (requestSecret !== secret) {
+    const msg = "wrong secret";
+    timelog(msg);
+    return c.text(msg, 403);
+  }
+  const payload: Payload = await c.req.json();
+  const { text, cw, visibility, localOnly, files } = payload.body.note;
+  if (localOnly) {
+    const msg = `post visibility: "local only"`;
+    timelog(msg);
+    return c.text(msg, 403);
+  }
+  if (visibility !== "public" && visibility !== "home") {
+    const msg = `post visibility: "${visibility}"`;
+    timelog(msg);
+    return c.text(msg, 403);
+  }
+  const uploadableFiles = (files ?? []).filter((f) =>
+    f.type.includes("image") || f.type === "video/mp4"
+  );
+  const [uploadFiles, unuploadFiles] = [
+    uploadableFiles.slice(0, 4),
+    uploadableFiles.slice(4),
+  ];
+  const mediaIds = await Promise.all(
+    uploadFiles.map(async (file) =>
+      await uploadMediaFromURL(file.url, file.type, authToken, ct0)
+    ),
+  );
 
-Deno.serve(app.fetch)
+  const filesStr: string = [
+    ...unuploadFiles,
+    ...(files ?? []).filter((f) =>
+      !(f.type.includes("image") || f.type === "video/mp4")
+    ),
+  ].reduce((p: string, c) => {
+    return `${p}\n${c.type.includes("image") ? "🖼" : "📄"} ${c.url}`;
+  }, "");
+  const tweetContent = (cw ? `${cw}...\n\n` : "") + (text ?? "") + filesStr;
+  if (!tweetContent) {
+    const msg = "empty content";
+    timelog(msg);
+    return c.text(msg, 403);
+  }
+  const res = await tweet(tweetContent, authToken, ct0, mediaIds);
+  if (res.status === 200) {
+    const msg = "successfully tweeted";
+    timelog(msg, tweetContent);
+    return c.text(msg, 200);
+  }
+  const msg = "error while tweeting";
+  timelog(msg, tweetContent);
+  return c.text(msg, 403);
+});
+
+Deno.serve(app.fetch);
--- types.ts
+++ types.ts
@@ -1,18 +1,51 @@
 export type Payload = {
-    body: {
-        note: Note
-    }
-}
+  body: {
+    note: Note;
+  };
+};
 
 type Note = {
-    text: string | null
-    cw: string | null
-    visibility: 'public' | 'home' | 'followers' | 'specified'
-    localOnly: boolean
-    files: File[]
-}
+  text: string | null;
+  cw: string | null;
+  visibility: "public" | "home" | "followers" | "specified";
+  localOnly: boolean;
+  files: File[];
+};
 
 type File = {
-    url: string
-    type: string
-}
+  url: string;
+  type: string;
+};
+
+export type CreateTweetRequest = {
+  variables: {
+    tweet_text: string;
+    media?: {
+      media_entities: {
+        media_id: string;
+      }[];
+      possibly_sensitive: boolean;
+    };
+  };
+  features: {
+    tweetypie_unmention_optimization_enabled: boolean;
+    responsive_web_edit_tweet_api_enabled: boolean;
+    graphql_is_translatable_rweb_tweet_is_translatable_enabled: boolean;
+    view_counts_everywhere_api_enabled: boolean;
+    longform_notetweets_consumption_enabled: boolean;
+    responsive_web_twitter_article_tweet_consumption_enabled: boolean;
+    tweet_awards_web_tipping_enabled: boolean;
+    longform_notetweets_rich_text_read_enabled: boolean;
+    longform_notetweets_inline_media_enabled: boolean;
+    responsive_web_graphql_exclude_directive_enabled: boolean;
+    verified_phone_label_enabled: boolean;
+    freedom_of_speech_not_reach_fetch_enabled: boolean;
+    standardized_nudges_misinfo: boolean;
+    tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled:
+      boolean;
+    responsive_web_media_download_video_enabled: boolean;
+    responsive_web_graphql_skip_user_profile_image_extensions_enabled: boolean;
+    responsive_web_graphql_timeline_navigation_enabled: boolean;
+    responsive_web_enhance_cards_enabled: boolean;
+  };
+};
